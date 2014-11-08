2e17a5c5cb5e3e9741406dfd3019281c619cae19
Nate Jones <nate@endot.org>
2014-11-08 16:53:12 -0500

    deploy properly for branches

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-rsync -avze 'ssh -p 22' site/ endot.org@endot.org:~/domains/docs.pmb.io/html/
+set -e
 
+if [[ $DRONE_BRANCH == "master" ]]; then
+  rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/docs.pmb.io/html/
+else
+    ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-docs.pmb.io/html/
+    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-docs.pmb.io/html/
+fi
