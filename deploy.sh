168d3c9a3acd1a188d395bf4804036ed183a48a3
Nate Jones <nate@endot.org>
2015-05-14 23:30:18 -0400

    extract domain name in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
+SITE=docs.pmb.io
 
 if [[ $DRONE_BRANCH == "master" ]]; then
-  rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/docs.pmb.io/html/
+    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/$SITE/html/
 else
-    ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-docs.pmb.io/html/
-    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-docs.pmb.io/html/
+    ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-$SITE/html/
+    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-$SITE/html/
 fi
