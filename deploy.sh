cdde8bce3a86ee1ace32162ecdcb39c497c1f3d4
Nate Jones <nate@endot.org>
2016-01-23 23:22:38 -0500

    deploy working on drone 0.4

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,17 @@
 set -e
 SITE=docs.pmb.io
 
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+echo "endot.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEA6t/F1UXfLPJxzuCsnYafDAWEyq9740FDPjN9o/fPDwn3lIMJafR4qjHYkrksI+8LKJc/xyvzZcxLLXPmK4Bl2Ui0s1VOsG/1jRmLqql1jMU+eMVeBUIgsc1qRyg3o/sVjpwgDjE7EUNRIfCaU5yivJZRsAqkFkJ6fDx4puMzPQU=" > /root/.ssh/known_hosts
+echo -e $PRIV_KEY | tr -d '"' > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
 if [[ $DRONE_BRANCH == "master" ]]; then
-    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/$SITE/html/
+  echo "Syncing to main site $SITE"
+  rsync -v -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/$SITE/html/
 else
-    ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-$SITE/html/
-    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-$SITE/html/
+  echo "Syncing to branch site ${DRONE_BRANCH}-$SITE"
+  ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-$SITE/html/
+  rsync -v -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-$SITE/html/
 fi
